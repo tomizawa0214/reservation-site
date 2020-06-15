e5e7b6c19170b2b9f15b8258bf980782f4f17301
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-15 23:58:03 +0900

    プロフィール情報変更

--- accounts/models.py
+++ accounts/models.py
@@ -4,61 +4,61 @@ from django.contrib.auth.models import UserManager, PermissionsMixin
 from django.utils import timezone
 
 class UserManager(UserManager):
-  def _create_user(self, email, password, **extra_fields):
-    email = self.normalize_email(email)
-    user = self.model(email=email, **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
-    return user
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-  def create_user(self, email, password=None, **extra_fields):
-    extra_fields.setdefault('is_staff', False)
-    extra_fields.setdefault('is_superuser', False)
-    return self._create_user(email, password, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
 
-  def create_superuser(self, email, password, **extra_fields):
-    extra_fields.setdefault('is_staff', True)
-    extra_fields.setdefault('is_superuser', True)
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-    if extra_fields.get('is_staff') is not True:
-      raise ValueError('Superuser must have is_staff=True.')
-    if extra_fields.get('is_superuser') is not True:
-      raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
 
-    return self._create_user(email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-  email = models.EmailField('メールアドレス', unique=True)
-  first_name = models.CharField(('姓'), max_length=30)
-  last_name = models.CharField(('名'), max_length=30)
-  department = models.CharField(('所属'), max_length=30, blank=True)
-  created = models.DateTimeField(('入会日'), default=timezone.now)
+    email = models.EmailField('メールアドレス', unique=True)
+    first_name = models.CharField(('姓'), max_length=30)
+    last_name = models.CharField(('名'), max_length=30)
+    description = models.TextField('自己紹介', default="", blank=True)
+    image = models.ImageField(upload_to='images', verbose_name='プロフィール画像', null=True, blank=True)
 
-  is_staff = models.BooleanField(
-    ('staff status'),
-    default=False,
-    help_text=('Designates whether the user can log into this admin site.'),
-  )
-  is_active = models.BooleanField(
-    ('active'),
-    default=True,
-    help_text=(
-      'Designates whether this user should be treated as active. '
-      'Unselect this instead of deleting accounts.'
-    ),
-  )
+    is_staff = models.BooleanField(
+        ('staff status'),
+        default=False,
+        help_text=('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        ('active'),
+        default=True,
+        help_text=(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
 
-  objects = UserManager()
+    objects = UserManager()
 
-  USERNAME_FIELD = 'email'
-  EMAIL_FIELD = 'email'
-  REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
-  class Meta:
-    verbose_name = ('user')
-    verbose_name_plural = ('users')
+    class Meta:
+        verbose_name = ('user')
+        verbose_name_plural = ('users')
 
-  def clean(self):
-    super().clean()
-    self.email = self.__class__.objects.normalize_email(self.email)
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
 
--- accounts/views.py
+++ accounts/views.py
@@ -6,54 +6,58 @@ from django.shortcuts import render, redirect
 from allauth.account import views
 
 class SignupView(views.SignupView):
-  template_name = 'accounts/signup.html'
-  form_class = SignupUserForm
+    template_name = 'accounts/signup.html'
+    form_class = SignupUserForm
 
 class LogoutView(views.LogoutView):
-  template_name = 'accounts/logout.html'
+    template_name = 'accounts/logout.html'
 
-  def post(self, *args, **kwargs):
-    if self.request.user.is_authenticated:
-      self.logout()
-    return redirect('/')
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            self.logout()
+        return redirect('/')
 
 class LoginView(views.LoginView):
-  template_name = 'accounts/login.html'
+    template_name = 'accounts/login.html'
 
 class ProfileEditView(LoginRequiredMixin, View):
-  def get(self, request, *args, **kwargs):
-    user_data = CustomUser.objects.get(id=request.user.id)
-    form = ProfileForm(
-      request.POST or None,
-      initial={
-        'first_name': user_data.first_name,
-        'last_name': user_data.last_name,
-        'department': user_data.department
-      }
-    )
+    def get(self, request, *args, **kwargs):
+        user_data = CustomUser.objects.get(id=request.user.id)
+        form = ProfileForm(
+            request.POST or None,
+            initial={
+                'first_name': user_data.first_name,
+                'last_name': user_data.last_name,
+                'description': user_data.description,
+                'image': user_data.image
+            }
+        )
 
-    return render(request, 'accounts/profile_edit.html', {
-      'form': form
-    })
-  
-  def post(self, request, *args, **kwargs):
-    form = ProfileForm(request.POST or None)
-    if form.is_valid():
-      user_data = CustomUser.objects.get(id=request.user.id)
-      user_data.first_name = form.cleaned_data['first_name']
-      user_data.last_name = form.cleaned_data['last_name']
-      user_data.department = form.cleaned_data['department']
-      user_data.save()
-      return redirect('profile')
+        return render(request, 'accounts/profile_edit.html', {
+            'form': form,
+            'user_data': user_data
+        })
 
-    return render(request, 'accounts/profile.html', {
-      'form': form
-    })
+    def post(self, request, *args, **kwargs):
+        form = ProfileForm(request.POST or None)
+        if form.is_valid():
+            user_data = CustomUser.objects.get(id=request.user.id)
+            user_data.first_name = form.cleaned_data['first_name']
+            user_data.last_name = form.cleaned_data['last_name']
+            user_data.description = form.cleaned_data['description']
+            if request.FILES.get('image'):
+                user_data.image = request.FILES.get('image')
+            user_data.save()
+            return redirect('profile')
+
+        return render(request, 'accounts/profile.html', {
+            'form': form
+        })
 
 class ProfileView(LoginRequiredMixin, View):
-  def get(self, request, *args, **kwargs):
-    user_data = CustomUser.objects.get(id=request.user.id)
+    def get(self, request, *args, **kwargs):
+        user_data = CustomUser.objects.get(id=request.user.id)
 
-    return render(request, 'accounts/profile.html', {
-      'user_data': user_data,
-    })
+        return render(request, 'accounts/profile.html', {
+            'user_data': user_data,
+        })
