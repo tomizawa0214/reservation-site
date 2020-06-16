c272e379c996f8a8b12e2241a431a9128d02130a
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-16 13:00:33 +0900

    店舗リスト追加

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from app.models import Store
+from django.views.generic import View
+from django.shortcuts import render
 
-class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = "app/index.html"
-    login_url = '/accounts/login/'
+class StoreView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = Store.objects.all()
+
+        return render(request, 'app/store.html', {
+            'store_data': store_data,
+        })
