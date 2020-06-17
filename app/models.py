559f9f07aa199bf908accd2f244819688dd6a5c7
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-17 23:17:57 +0900

    スタッフリスト追加

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import CustomUser
 
 class Store(models.Model):
     name = models.CharField('店舗', max_length=100)
@@ -9,3 +10,10 @@ class Store(models.Model):
 
     def __str__(self):
         return self.name
+
+class Staff(models.Model):
+    user = models.OneToOneField(CustomUser, verbose_name='スタッフ', on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, verbose_name='店舗', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.store}:{self.user}'
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from app.models import Store
+from app.models import Store, Staff
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 class StoreView(View):
     def get(self, request, *args, **kwargs):
@@ -8,4 +8,14 @@ class StoreView(View):
 
         return render(request, 'app/store.html', {
             'store_data': store_data,
+        })
+
+class StaffView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = get_object_or_404(Store, id=self.kwargs['pk'])
+        staff_data = Staff.objects.filter(store=store_data).select_related('user')
+
+        return render(request, 'app/staff.html', {
+            'store_data': store_data,
+            'staff_data': staff_data,
         })
