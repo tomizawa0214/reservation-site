78ed7a3fb5908369f47c640a4aa49383318a1d5d
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-19 00:15:02 +0900

    予約機能追加

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from datetime import datetime, date, timedelta, time
 from django.db.models import Q
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.timezone import localtime, make_aware
 from django.views.generic import View
-from app.models import Store, Staff
+from app.models import Store, Staff, Booking
+from app.forms import BookingForm
 
 class StoreView(View):
     def get(self, request, *args, **kwargs):
@@ -49,13 +50,13 @@ class CalendarView(View):
             calendar[hour] = row
         start_time = make_aware(datetime.combine(start_day, time(hour=10, minute=0, second=0)))
         end_time = make_aware(datetime.combine(end_day, time(hour=20, minute=0, second=0)))
-        # booking_date = Booking.objects.filter(staff=staff_data).exclude(Q(start__gt=end_time) | Q(end__lt=start_time))
-        # for booking in booking_data:
-        #     local_time = localtime(booking.start)
-        #     booking_date = local_time.date()
-        #     booking_hour = local_time.hour
-        #     if (booking_hour in calendar) and (booking_date in calendar[booking_hour]):
-        #         calendar[booking_hour][booking_date] = False
+        booking_data = Booking.objects.filter(staff=staff_data).exclude(Q(start__gt=end_time) | Q(end__lt=start_time))
+        for booking in booking_data:
+            local_time = localtime(booking.start)
+            booking_date = local_time.date()
+            booking_hour = local_time.hour
+            if (booking_hour in calendar) and (booking_date in calendar[booking_hour]):
+                calendar[booking_hour][booking_date] = False
 
         return render(request, 'app/calendar.html', {
             'staff_data': staff_data,
@@ -66,4 +67,56 @@ class CalendarView(View):
             'before': days[0] - timedelta(days=7),
             'next': days[-1] + timedelta(days=1),
             'today': today,
+        })
+
+class BookingView(View):
+    def get(self, request, *args, **kwargs):
+        staff_data = Staff.objects.filter(id=self.kwargs['pk']).select_related('user').select_related('store')[0]
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        day = self.kwargs.get('day')
+        hour = self.kwargs.get('hour')
+        form = BookingForm(request.POST or None)
+
+        return render(request, 'app/booking.html', {
+            'staff_data': staff_data,
+            'year': year,
+            'month': month,
+            'day': day,
+            'hour': hour,
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        staff_data = get_object_or_404(Staff, id=self.kwargs['pk'])
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        day = self.kwargs.get('day')
+        hour = self.kwargs.get('hour')
+        start_time = make_aware(datetime(year=year, month=month, day=day, hour=hour))
+        end_time = make_aware(datetime(year=year, month=month, day=day, hour=hour + 1))
+        booking_data = Booking.objects.filter(staff=staff_data, start=start_time)
+        form = BookingForm(request.POST or None)
+        if booking_data.exists():
+            form.add_error(None, '既に予約があります。\n別の日時で予約をお願いします。')
+        else:
+            if form.is_valid():
+                booking = Booking()
+                booking.staff = staff_data
+                booking.start = start_time
+                booking.end = end_time
+                booking.first_name = form.cleaned_data['first_name']
+                booking.last_name = form.cleaned_data['last_name']
+                booking.tel = form.cleaned_data['tel']
+                booking.remarks = form.cleaned_data['remarks']
+                booking.save()
+                return redirect('store')
+        
+        return render(request, 'app/booking.html', {
+            'staff_data': staff_data,
+            'year': year,
+            'month': month,
+            'day': day,
+            'hour': hour,
+            'form': form,
         })
