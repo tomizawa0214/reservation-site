fb908fa1ee02b493c6f943a73e134d61887fb566
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-19 10:19:38 +0900

    スタッフ専用ページ追加

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('calendar/<int:pk>/<int:year>/<int:month>/<int:day>/', views.CalendarView.as_view(), name='calendar'),
     path('booking/<int:pk>/<int:year>/<int:month>/<int:day>/<int:hour>/', views.BookingView.as_view(), name='booking'),
     path('thanks/', views.ThanksView.as_view(), name='thanks'),
+    path('mypage/<int:year>/<int:month>/<int:day>/', views.MyPageView.as_view(), name='mypage'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,11 +3,20 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.timezone import localtime, make_aware
 from django.views.generic import View, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from app.models import Store, Staff, Booking
 from app.forms import BookingForm
 
 class StoreView(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            start_date = date.today()
+            weekday = start_date.weekday()
+            # カレンダー日曜日開始
+            if weekday != 6:
+                start_date -= timedelta(days=weekday + 1)
+            return redirect('mypage', start_date.year, start_date.month, start_date.day)
+
         store_data = Store.objects.all()
 
         return render(request, 'app/store.html', {
@@ -123,3 +132,47 @@ class BookingView(View):
 
 class ThanksView(TemplateView):
     template_name = 'app/thanks.html'
+
+class MyPageView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        staff_data = Staff.objects.filter(id=request.user.id).select_related('user').select_related('store')[0]
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        day = self.kwargs.get('day')
+        # 週始め
+        start_date = date(year=year, month=month, day=day)
+        # 1週間
+        days = [start_date + timedelta(days=day) for day in range(7)]
+        start_day = days[0]
+        end_day = days[-1]
+
+        calendar = {}
+        # 10時～20時
+        for hour in range(10, 21):
+            row = {}
+            for day_ in days:
+                row[day_] = ""
+            calendar[hour] = row
+        start_time = make_aware(datetime.combine(start_day, time(hour=10, minute=0, second=0)))
+        end_time = make_aware(datetime.combine(end_day, time(hour=20, minute=0, second=0)))
+        booking_data = Booking.objects.filter(staff=staff_data).exclude(Q(start__gt=end_time) | Q(end__lt=start_time))
+        for booking in booking_data:
+            local_time = localtime(booking.start)
+            booking_date = local_time.date()
+            booking_hour = local_time.hour
+            if (booking_hour in calendar) and (booking_date in calendar[booking_hour]):
+                calendar[booking_hour][booking_date] = booking.first_name
+
+        return render(request, 'app/mypage.html', {
+            'staff_data': staff_data,
+            'booking_data': booking_data,
+            'calendar': calendar,
+            'days': days,
+            'start_day': start_day,
+            'end_day': end_day,
+            'before': days[0] - timedelta(days=7),
+            'next': days[-1] + timedelta(days=1),
+            'year': year,
+            'month': month,
+            'day': day,
+        })
